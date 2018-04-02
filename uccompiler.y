af7235f34dd4bc41363e6f7e145c90e84dc5826f
Diogo Amores <dingo@Diogos-MBP-3.lan>
2018-04-02 15:58:57 +0100

    Gramatica quase feita

--- uccompiler.y
+++ uccompiler.y
@@ -2,14 +2,16 @@
     #include <stdio.h>
     int yylex(void);
     void yyerror (const char *s);
+    char typing;
 %}
 
 %token CHAR ELSE IF INT SHORT DOUBLE RETURN VOID WHILE BITWISEAND BITWISEOR BITWISEXOR AND ASSIGN MUL COMMA DIV EQ GE GT LBRACE LE LT MINUS MOD NE NOT OR PLUS RBRACE RPAR LPAR SEMI REALLIT RESERVED CHRLIT ID INTLIT
+//FuncAndDeclarations DeclarationAndStates FuncDefinition FuncDeclaration Declaration TypeSpec FuncDeclarator FuncBody Statement ParamList ParamDeclaration Declarator Expr
+
 %left LPAR RPAR
 %right PLUS MINUS
 %right NOT
 %left MUL DIV MOD
-%left PLUS MINUS
 %left LE LT
 %left GT GE
 %left EQ NE
@@ -20,6 +22,89 @@
 %left OR
 %right ASSIGN
 %left COMMA
+
+%%
+FuncAndDeclarations:
+                    |FuncDefinition 
+                    |FuncDeclaration
+                    |Declaration
+                    |FuncDefinition FuncAndDeclarations
+                    |FuncDeclaration FuncAndDeclarations
+                    |Declaration FuncAndDeclarations
+                    ;
+
+FuncDefinition: TypeSpec FuncDeclarator FuncBody
+                ;
+
+FuncBody:
+            |LBRACE DeclarationAndStates RBRACE
+            |LBRACE RBRACE
+            ;
+
+DeclarationAndStates:
+                        |Statement DeclarationAndStates
+                        |Declaration DeclarationAndStates
+                        |Statement
+                        |Declaration
+                        ;
+
+FuncDeclaration: TypeSpec FuncDeclaration SEMI
+                    ;
+
+FuncDeclarator: ID LPAR ParamList RPAR
+                    ;
+
+ParamList:
+                    |ParamDeclaration
+                    |ParamDeclaration COMMA ParamDeclaration
+                    ;
+
+ParamDeclaration:
+                    |TypeSpec
+                    |TypeSpec ID
+                    ;
+
+Declaration:
+                    |TypeSpec Declarator SEMI
+                    |TypeSpec Declarator COMMA Declarator
+                    ;
+
+TypeSpec:
+                    |CHAR
+                    |INT
+                    |VOID
+                    |DOUBLE
+                    |SHORT
+                    ;
+
+Declarator:
+                    |ID
+                    |ID ASSIGN Expr
+                    ;
+
+Statement:
+                    |SEMI
+                    |Expr SEMI
+                    |LBRACE RBRACE
+                    |LBRACE Statement RBRACE
+                    |IF LPAR Expr RPAR Statement
+                    |IF LPAR Expr RPAR Statement ELSE Statement
+                    |WHILE LPAR Expr RPAR Statement
+                    |RETURN SEMI
+                    |RETURN Expr SEMI
+                    ;
+
+Expr:
+                    |Expr ASSIGN Expr
+                    |Expr COMMA Expr
+
+
+
+
+
+
+
+%%
 
 
 int main() {
---
+++ y.tab.h
@@ -0,0 +1,133 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     CHAR = 258,
+     ELSE = 259,
+     IF = 260,
+     INT = 261,
+     SHORT = 262,
+     DOUBLE = 263,
+     RETURN = 264,
+     VOID = 265,
+     WHILE = 266,
+     BITWISEAND = 267,
+     BITWISEOR = 268,
+     BITWISEXOR = 269,
+     AND = 270,
+     ASSIGN = 271,
+     MUL = 272,
+     COMMA = 273,
+     DIV = 274,
+     EQ = 275,
+     GE = 276,
+     GT = 277,
+     LBRACE = 278,
+     LE = 279,
+     LT = 280,
+     MINUS = 281,
+     MOD = 282,
+     NE = 283,
+     NOT = 284,
+     OR = 285,
+     PLUS = 286,
+     RBRACE = 287,
+     RPAR = 288,
+     LPAR = 289,
+     SEMI = 290,
+     REALLIT = 291,
+     RESERVED = 292,
+     CHRLIT = 293,
+     ID = 294,
+     INTLIT = 295
+   };
+#endif
+/* Tokens.  */
+#define CHAR 258
+#define ELSE 259
+#define IF 260
+#define INT 261
+#define SHORT 262
+#define DOUBLE 263
+#define RETURN 264
+#define VOID 265
+#define WHILE 266
+#define BITWISEAND 267
+#define BITWISEOR 268
+#define BITWISEXOR 269
+#define AND 270
+#define ASSIGN 271
+#define MUL 272
+#define COMMA 273
+#define DIV 274
+#define EQ 275
+#define GE 276
+#define GT 277
+#define LBRACE 278
+#define LE 279
+#define LT 280
+#define MINUS 281
+#define MOD 282
+#define NE 283
+#define NOT 284
+#define OR 285
+#define PLUS 286
+#define RBRACE 287
+#define RPAR 288
+#define LPAR 289
+#define SEMI 290
+#define REALLIT 291
+#define RESERVED 292
+#define CHRLIT 293
+#define ID 294
+#define INTLIT 295
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
